5948cf276a8213b8517550b73fefc92ed13d5124
Timothy Peng <tpeng@Timothys-MacBook-Pro.local>
2020-01-11 15:27:45 -0500

    modify JS, scrolling

--- home.js
+++ home.js
@@ -1,1 +1,18 @@
-alert("connected");
+//variables, functions
+
+let navbarMobile = function() {
+  let pageWidth = parseInt(document.body.offsetWidth);
+
+  $(window).resize(function() {
+    pageWidth = parseInt(document.body.offsetWidth);
+    if (pageWidth < 580) {
+      $('#navbar').removeClass('sticky-top');
+    } else if (pageWidth > 580) {
+      $('#navbar').addClass('sticky-top')
+    }
+  })
+  
+};
+
+navbarMobile();
+
